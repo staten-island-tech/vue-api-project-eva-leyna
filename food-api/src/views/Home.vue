b8618fa8733ecf5bd7a003b3adf5f10d16d273e7
ctrlLey <71617500+ctrlLey@users.noreply.github.com>
2021-11-15 12:28:05 -0500

    Update Home.vue

--- food-api/src/views/Home.vue
+++ food-api/src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <header class="header">
+      <h1 class="title">Spoonacular</h1>
+      <div class="cart">Cart (0)</div>
+    </header>
+    <h3>View Recipes and Buy Food</h3>
+    <form class="search">
+      <input class="search-input" type="text" placeholder=" Search..." name="search">
+    </form>
     
   </div>
 </template>
@@ -16,3 +23,32 @@ export default {
   },
 };
 </script>
+
+<style>
+ body {
+   margin: 0;
+ }
+ .header {
+   width: 100%;
+   height: 10vh;
+   display: flex;
+   flex-direction: row;
+   justify-content: center;
+   align-items: center;
+   background-color: bisque;
+   }
+  .cart {
+    width: 10%;
+    height: 90%;
+    margin: 0 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: .1rem black solid;
+   }
+  .title {
+    width: 80%;
+    margin: auto 3rem;
+    text-align: center;
+  }
+</style>
